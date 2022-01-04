d1bb8b27f39b2258f62661cf0beb2d62d8084736
iqbal12qwas <iqbal12qwas@gmail.com>
2022-01-04 10:45:08 +0700

    Update MySQL

---
+++ shoppingdb.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2022 at 04:43 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoppingdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopping`
+--
+
+CREATE TABLE `shopping` (
+  `id` bigint(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `createddate` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shopping`
+--
+
+INSERT INTO `shopping` (`id`, `name`, `createddate`) VALUES
+(2, 'new shoppingggg', '2019-11-20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `postcode` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `phone`, `country`, `city`, `postcode`, `name`, `address`) VALUES
+(1, 'test', '$2a$12$jxcFtSTzRMcRPn6JjfLONOvz9W1Bsa/cLEnNtwYih7bH25l0UcqUi', 'test@coding.test', 123456, 'Indonesia', 'Bandung', 12345, 'postman', 'Dago'),
+(2, 'iqbal', '$2a$10$/X2K2A2BmeNN2xmtdFTNC.uRIaw.7vFtkDZ5zbB6M.4Oz5GKvZvdq', 'iqbal@iqbal@gmail.com', 123456, 'Indonesia', 'Kota Pontianak', 78243, 'Bale Bale', 'Siantan Hilir');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopping`
+--
+ALTER TABLE `shopping`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopping`
+--
+ALTER TABLE `shopping`
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
